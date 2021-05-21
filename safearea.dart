60dff1a237b99709f0ecbbe3b7277ca1dc152c0d
Roshan Badwaik <33425706+roshan-codes@users.noreply.github.com>
2021-05-21 15:56:47 +0530

    Create safearea.dart

---
+++ safearea.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: BodyWidget(),
+      ),
+    );
+  }
+}
+
+class BodyWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Align(
+      alignment: Alignment.topLeft,
+      child: SafeArea(
+        left: true,
+        top: true,
+        right: true,
+        bottom: true,
+        minimum: const EdgeInsets.all(16.0),
+        child: Text(
+            'My Widget: This is my widget.'),
+      ),
+    );
+  }
+}
